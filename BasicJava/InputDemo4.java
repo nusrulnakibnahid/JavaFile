0b9f10f4f61fd42cc315b4f1c3d8b47eb33cfe33
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-06 22:44:41 +0600

    first commit

---
+++ BasicJava/InputDemo4.java
@@ -0,0 +1,17 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class InputDemo4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double number;
+
+        System.out.print("Enter any number = ");
+        number = input.nextDouble();
+
+        System.out.print("Number is = "+number);
+    }
+    
+}
