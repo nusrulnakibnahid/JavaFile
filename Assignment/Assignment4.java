0fd114a832817e33e4ec8108c26d510523d6f503
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-11 23:42:59 +0600

    first commit

---
+++ Assignment/Assignment4.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Assignment4 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int PhonePrice = 1800; //1800 euros
+        System.out.println("Phone price is = "+PhonePrice);
+
+        int numberOfInstallment,installmentPerMonth;
+
+        System.out.print("Enter Number Of Installment= ");
+        numberOfInstallment = input.nextInt();
+        System.out.println("Number Of Installment = "+numberOfInstallment);
+
+        installmentPerMonth = PhonePrice/numberOfInstallment;
+
+        System.out.println("Permonth Installment= $"+installmentPerMonth);
+
+    }
+}
