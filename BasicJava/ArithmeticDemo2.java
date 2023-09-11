9ce21c89223fa16681ddce1d27e736eb2a3116bb
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-11 23:06:00 +0600

    first commit

---
+++ BasicJava/ArithmeticDemo2.java
@@ -0,0 +1,36 @@
+package BasicJava;
+
+import java.util.Scanner;
+//user input arithmetic demo
+public class ArithmeticDemo2 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int a,b,result;
+
+        System.out.print("Enter num1 = ");
+        a = input.nextInt();
+
+        System.out.print("Enter b= ");
+        b = input.nextInt();
+
+        result = a + b;
+        System.out.println("Summation =  "+result);
+
+        result = a - b;
+        System.out.println("Subtraction =  "+result);
+
+        result = a * b;
+        System.out.println("Multiplication =  "+result);
+
+        result = a / b;
+        System.out.println("Division =  "+result);
+
+        result = a % b;
+        System.out.print("Remainder =  "+result);
+ 
+
+    }
+    
+}
