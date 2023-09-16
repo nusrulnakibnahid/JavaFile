dc72d9bb4ae12545dc3902e12f24adfa1afd6f7d
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-16 22:20:18 +0600

    first commit

---
+++ BasicJava/TriangleDemo.java
@@ -0,0 +1,24 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class TriangleDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        double base,height,area;
+        
+
+        System.out.print("Enter Base: ");
+        base = input.nextDouble();
+
+        System.out.print("Enter Height: "); 
+        height = input.nextDouble(); 
+
+        area = 0.5 * base * height; 
+
+        System.out.println("Area of a triangle: "+area); 
+
+    
+    }
+    
+}
