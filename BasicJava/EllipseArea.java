31ef64c4e436591678eb3c90862f9e5eecc04bda
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-18 21:57:50 +0600

    first commit

---
+++ BasicJava/EllipseArea.java
@@ -0,0 +1,23 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class EllipseArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double x,y,area;  /*x= radius of the major axis, y= radius of the minor axis */
+
+        System.out.print("Enter x = ");
+        x = input.nextDouble();
+
+        System.out.print("Enter y = ");
+        y = input.nextDouble();
+
+
+        area = 3.1416 * x * y;  // Formula Area of Ellipse = pi * a *b 
+
+        System.out.print("Area of Ellipse is : " +area );
+
+    }
+}
