e56a98789083b610d1205307bf0ff2c95c164b8b
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-18 22:24:09 +0600

    first commit

---
+++ BasicJava/KitePerimeter.java
@@ -0,0 +1,23 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class KitePerimeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double x,y,kite_perimeter;              /*x = Length of the first pair of equal sides
+                                                  y = Length of the second pair of equal sides */
+
+        System.out.print("Enter x: " ); 
+        x = input.nextDouble(); 
+
+        System.out.print("Enter y: " ); 
+        y = input.nextDouble();  
+        
+        kite_perimeter = (2*x) + (2*y);  //Formula (Kite Perimeter=2a + 2b)
+
+        System.out.print("Kite Perimeter is : " +kite_perimeter);
+    
+    }
+}
