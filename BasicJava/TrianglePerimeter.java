d662bb6f01e5e8b36a71200812ceeac563eed0df
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-18 22:14:51 +0600

    first commit

---
+++ BasicJava/TrianglePerimeter.java
@@ -0,0 +1,26 @@
+package BasicJava;
+
+import java.util.Scanner;
+//Perimeter of a Triangle
+public class TrianglePerimeter {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double a,b,c,triangle; //a, b and c being the side lengths
+
+        System.out.print("Enter a: ");
+        a = input.nextDouble(); 
+
+        System.out.print("Enter b: "); 
+        b = input.nextDouble(); 
+
+        System.out.print("Enter c: "); 
+        c = input.nextDouble();  
+
+        triangle = a + b + c;   //Formula (Triangle	a + b + c)
+
+        System.out.println("Perimeter of a triangle is : "  +triangle);
+
+    }
+}
