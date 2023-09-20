a85f3404b8c5a5c74363d05637742356bdf1c41e
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-20 22:35:45 +0600

    first commit

---
+++ Beecrowd_Problem_Solving/Hello.java
@@ -0,0 +1,12 @@
+package Beecrowd_Problem_Solving;
+
+import java.io.IOException;
+
+public class Hello {
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
