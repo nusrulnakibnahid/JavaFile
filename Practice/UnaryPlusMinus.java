307dca5d4801e3c7d00f949e3e0afefb54ba77a2
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-23 21:58:18 +0600

    commit

---
+++ Practice/UnaryPlusMinus.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class UnaryPlusMinus {
+    public static void main(String[] args) {
+        
+        int x = -10;
+        int result;
+
+        result = +x;
+        System.out.println("Result in unary Plus: "+result);
+
+        result = -x;
+        System.out.print("Result in unary Minus: "+result);
+        result = -x; 
+    }
+}
