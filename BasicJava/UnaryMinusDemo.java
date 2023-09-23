58b6805670dfadd17679a3ee514a499b4b7bebff
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-23 21:54:29 +0600

    commit

---
+++ BasicJava/UnaryMinusDemo.java
@@ -0,0 +1,13 @@
+package BasicJava;
+
+public class UnaryMinusDemo {
+    public static void main(String[] args) {
+        
+        int x =20;
+        int result;
+
+        result = -x; //Unary Minus Operator
+
+        System.out.println("Result is: "+result);
+    }
+}
---
+++ BasicJava/UnaryPlusDemo.java
@@ -0,0 +1,14 @@
+package BasicJava;
+
+public class UnaryPlusDemo {
+    public static void main(String[] args) {
+       int x = 10;
+       int result;
+
+       result = +x; // Unary plus operator
+
+       System.out.println("Result is: " +result);
+
+
+    }
+}
