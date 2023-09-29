6b4eda9633d852433c1fb24cea39e3074a80da76
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-09-29 21:59:27 +0600

    commit

---
+++ BasicJava/CapitalSmall.java
@@ -0,0 +1,173 @@
+package BasicJava;
+
+import java.util.Scanner;
+//Find capital or small letter
+public class CapitalSmall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        char ch;
+        System.out.print("Enter any letter: ");
+        ch = input.next().charAt(0); 
+
+        if (ch=='a') {
+            System.out.println("Small letter");
+        }
+        else if (ch=='A') {
+            System.out.println("Capital letter");
+        }
+        else if (ch=='b'){
+            System.out.println("Small letter");
+        }
+        else if (ch=='B'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='c'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='C'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='d') {
+            System.out.println("Small letter");
+        }
+        else if(ch=='D'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='e'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='E'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='f'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='F'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='g'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='G'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='h') {
+            System.out.println("Small letter");
+        }
+        else if (ch=='H') {
+            System.out.println("Capital letter");
+        }
+        else if (ch=='i'){
+            System.out.println("Small letter");
+        }
+        else if (ch=='I') {
+            System.out.println("Capital letter");
+        }
+        else if (ch=='j'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='J'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='k'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='K'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='l'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='L'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='m'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='M'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='n'){
+            System.out.println("Small letter");
+        }
+        else if (ch=='N'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='o'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='O'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='p'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='P'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='q'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='Q'){
+            System.out.println("Capital letter");
+        }
+        else if (ch=='r'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='R'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='s'){
+            System.out.println("Small letter");
+        }
+        else if (ch=='S'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='t'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='T'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='u'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='U'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='v'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='V'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='w'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='W'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='x'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='X'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='y'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='Y'){
+            System.out.println("Capital letter");
+        }
+        else if(ch=='z'){
+            System.out.println("Small letter");
+        }
+        else if(ch=='Z'){
+            System.out.println("Capital letter");
+        }
+        else {
+            System.out.println("Invalid action!");
+        }
+    }
+}
