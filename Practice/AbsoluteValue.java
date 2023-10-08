7fea6c425ce184664b0a685af1bce76ed8001129
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-08 21:09:57 +0600

    commit

---
+++ Practice/AbsoluteValue.java
@@ -0,0 +1,26 @@
+package Practice;
+
+import java.util.Scanner;
+//Find the absolute value of a number entered through the keyboard
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+
+        num = (-1)*num;
+
+        if(num<0) {
+            System.out.println("Absolute number");
+        }
+
+        else {
+            System.out.print("Not Absolute number");
+        }
+
+
+
+    }
+}
