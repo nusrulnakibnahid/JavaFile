331bb11352bbf9431431a08224956435d1bda979
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-12 21:19:20 +0600

    commit

---
+++ Practice/SpecifiedExpression.java
@@ -0,0 +1,9 @@
+package Practice;
+//Write a Java program to compute the specified expressions and print the output
+public class SpecifiedExpression {
+    public static void main(String[] args) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        
+    }
+}
