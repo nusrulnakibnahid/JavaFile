21b82bb4026261419288e92984f20b432a20756f
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-28 22:49:06 +0600

    commit

---
+++ BasicJava/BitwiseOperatorDemo2.java
@@ -0,0 +1,19 @@
+package BasicJava;
+//Bitwise Shift Right(>>),Shift Left(<<) demo
+import java.util.Scanner;
+
+public class BitwiseOperatorDemo2 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+
+        int a,b;
+        System.out.print("Enter a: ");
+        a = input.nextInt();
+
+        b = a>>3;
+        System.out.println("a>> = "+b);
+
+        b = a<<3;
+        System.out.println("a<< = "+b);
+    }
+}
