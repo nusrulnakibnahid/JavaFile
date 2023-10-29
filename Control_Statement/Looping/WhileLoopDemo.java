8b7fc212c1d7af176360164dcf1428deba1f82a0
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-29 22:56:34 +0600

    commit

---
+++ Control_Statement/Looping/WhileLoopDemo.java
@@ -0,0 +1,26 @@
+package Control_Statement.Looping;
+
+public class WhileLoopDemo {
+    public static void main(String[] args){
+
+        int i = 1;
+        while(i<=50){
+            System.out.println(i+" Hi Coder ");
+            i++;
+        }
+
+        int j =1; //odd number
+        while(j<=50){
+            System.out.println(j);
+            j =j+2;
+        }
+
+        int k =0;
+        while(k<=50){ //even number
+            System.out.println(k);
+            k =k+2;
+        }
+
+
+    }
+}
