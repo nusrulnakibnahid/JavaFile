1d0708181aa4d1ff0ceb2a07826ce94a9096ac27
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-29 22:38:43 +0600

    commit

---
+++ Control_Statement/Looping/ForLoopDemo2.java
@@ -0,0 +1,10 @@
+package Control_Statement.Looping;
+
+public class ForLoopDemo2 {
+    public static void main(String[] args) {
+
+        for (int i = 50; i>=1; i--){ //decrement the loop counter
+            System.out.println(i);
+        }
+    }
+}
