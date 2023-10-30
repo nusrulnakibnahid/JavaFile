bef7de4db978c310f9f8d20915350cacae879e79
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-30 22:06:39 +0600

    commit

---
+++ Assignment/Assignment10.java
@@ -0,0 +1,16 @@
+package Assignment;
+// find sum of 1-10 using for loop
+public class Assignment10 {
+       public static void main(String[] args) {
+
+        int x,sum=0;
+        for(x=1;x<=10;x++){
+        sum=sum+x;
+       }
+
+    System.out.println("Sum(1-10):"+sum);
+
+        
+    }
+    
+}
