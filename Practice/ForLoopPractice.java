e70afd40ccbf94185241c2872fd3866d4ec586f4
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-10-31 20:59:54 +0600

    commit

---
+++ Practice/ForLoopPractice.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class ForLoopPractice {
+    public static void main(String[] args) {
+        
+        //addition
+        int a,sum=0;
+        for(a=1;a<=10;a++){
+        sum=sum+a;
+        }
+            System.out.println("Addition(1-10):"+sum);
+
+
+        //Subtraction
+        int b,sub=0;
+        for(b=1;b<=20;b++){
+            sub = sub - b;
+        }    
+        System.out.println("Subtraction(1-20):"+sub);
+
+
+    }
+}
