753db4453e3172589cf76c858119a3ebdc18a38e
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-11-05 22:25:18 +0600

    commit

---
+++ Assignment/Assignment12.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+import java.util.Scanner;
+// print sum of odd numbers from m-n
+public class Assignment12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int m,n,sum=0;
+        System.out.print("Enter m:");
+        m = input.nextInt();
+        System.out.print("Enter n:");
+        n = input.nextInt();
+
+        for(int i=m; i<=n; i=i+2) {
+            System.out.println(i);
+            sum = sum+i;
+        }
+
+        System.out.print("Sum:"+sum);
+
+    }
+}
