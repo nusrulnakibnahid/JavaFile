4c9b6911e25b851647e2b79316becf272bf99200
nu7srulnakibnahid.mail@gmail.com <nusrulnakibnahid.mail@gmail.com>
2023-11-12 21:27:24 +0600

    commit

---
+++ BasicJava/PalindromeDemo.java
@@ -0,0 +1,30 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class PalindromeDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+
+        int sum=0, num,r,temp;
+
+        System.out.print("Enter a number:");
+        num = input.nextInt(); 
+
+        temp = num;
+
+        while(temp!= 0){
+            r = temp % 10;
+            sum = sum*10 +r;
+            temp = temp/10;
+        }
+
+        System.out.println("Answer:"+sum);
+
+        if(num == sum){
+            System.out.println("Palindrome Number");
+        }else{
+            System.out.println("Not a palindrome number"); 
+        }
+    }
+}
