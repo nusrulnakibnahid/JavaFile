71ff7fe6e346ae9be1bfe2c0d16e124d041e8052
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmailcom>
2024-01-12 00:09:28 +0600

    commit

---
+++ Revision/product.java
@@ -0,0 +1,27 @@
+package Revision;
+
+/*
+ * Assignment-1
+ * create a class called Product
+ * create a main method
+ * print the following data in main method
+
+
+ id: 101,
+title: iphone15,
+price: 1895 euros,
+description: perfect product with best image quality,
+category: phone,
+ */
+public class product {
+    public static void main(String[] args) {
+
+        System.out.println("id: 101,");
+        System.out.println("title: iphone15");
+        System.out.println("price: 1895 euros");
+        System.out.println("description: perfect product with best image quality");
+        System.out.println("category: phone");
+
+    }
+
+}
