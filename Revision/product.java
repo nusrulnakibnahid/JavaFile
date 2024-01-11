8188ccf0b0ec27f5990b8234b8eee9e8c80cc1a1
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-12 00:21:55 +0600

    commit

--- Revision/product.java
+++ Revision/product.java
@@ -16,8 +16,8 @@ category: phone,
 public class product {
     public static void main(String[] args) {
 
-        System.out.println("id: 101,");
-        System.out.println("title: iphone15");
+        System.out.println("id: 101");
+        System.out.println("title: iphone15"); 
         System.out.println("price: 1895 euros");
         System.out.println("description: perfect product with best image quality");
         System.out.println("category: phone");
