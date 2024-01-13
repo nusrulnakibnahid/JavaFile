c0c8ccf39e9d28c437f4eae96a2ac0b3a4c9f8cc
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-13 22:21:55 +0600

    commit

--- Revision/Asg5.java
+++ Revision/Asg5.java
@@ -1,27 +1,26 @@
 package Revision;
 
 import java.util.Scanner;
-
+/*
+ * valid voter program using if,else 
+ * step 1: ask for a person age
+ * step 2: if age is equal or more than 18 than print valid voter
+ * step 3: else print invalid voter
+ */
 public class Asg5 {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in); 
 
-        int phone_price = 1800; // 1800 euros
-
-        int numberOfInstallment, installmentPerMonth;
-
-
-        System.out.print("Number of Installment: ");
-        numberOfInstallment = input.nextInt();
-
-        System.out.print("Installment Per Month: ");
-        installmentPerMonth = input.nextInt(); 
-
-        installmentPerMonth = phone_price / numberOfInstallment; 
-
-        System.out.print("Monthly installment amount: "+installmentPerMonth);
+        int age;
+        System.out.print("Enter your age: "); 
+        age = input.nextInt(); 
 
 
+        if (age >= 18){
+            System.out.println("Valid Voter");
+        }
+        else {
+            System.out.println("Invalid Voter");
+        }
     }
-    
 }
