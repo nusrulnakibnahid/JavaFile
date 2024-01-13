6c7d0a00caaa838694e61275fc5da89d2d9e09c7
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-13 22:28:31 +0600

    commit

---
+++ Revision/EvenOddCheck.java
@@ -0,0 +1,24 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x; 
+        System.out.print("Enter a number: "); 
+        x = input.nextInt();
+
+
+if (x % 2 == 0) {
+
+            System.out.println("Even number");
+        }
+
+        else {
+            System.out.println("Odd number");
+        }
+    
+    }
+}
