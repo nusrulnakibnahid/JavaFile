3be0d8be171712d88bac11ec4a66d31b217f502e
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-14 23:02:43 +0600

    commit

---
+++ Revision/ForLoop.java
@@ -0,0 +1,17 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        System.out.print("Enter how many time you want the output: ");
+        num = input.nextInt(); 
+
+        for(int i=1; i<=num; i++){
+            System.out.println(i+ " Assalamu-alaikum ");
+        }
+    }
+}
