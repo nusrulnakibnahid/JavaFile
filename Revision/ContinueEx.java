3d30451e17a1effeebc4d24cff1ebc7d245e48b8
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-23 22:23:34 +0600

    commit

---
+++ Revision/ContinueEx.java
@@ -0,0 +1,13 @@
+package Revision;
+
+public class ContinueEx {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i= i+3){
+            if (i == 10){
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+    }
+}
