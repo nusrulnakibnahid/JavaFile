453a91b9ad014d0531b7725dd32d3e3d69cf861f
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-24 22:58:56 +0600

    commit

---
+++ Revision/ContinueEx2.java
@@ -0,0 +1,25 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class ContinueEx2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int m, n, sum = 0;
+
+        System.out.print("Enter first number:");
+        m = input.nextInt();
+
+        System.out.print("Enter second number:");
+        n = input.nextInt();
+
+        for (int i = m; i < n; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("Total of even number:" + sum);
+    }
+}
