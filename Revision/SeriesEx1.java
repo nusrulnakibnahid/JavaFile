f21d07a841e7eddfb42d4f86efbb0c00e643bcb1
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-26 21:26:30 +0600

    commit

---
+++ Revision/SeriesEx1.java
@@ -0,0 +1,22 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class SeriesEx1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n, sum = 0;
+
+        System.out.print("Enter last number: ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+            sum = sum + i;
+        }
+        System.out.println();
+        System.out.print("Total:" + sum);
+
+    }
+}
