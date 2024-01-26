bd2e7fd4281d6c0846f48488da0d6af3ec9fb915
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-26 21:14:34 +0600

    commit

---
+++ Revision/Sum1to10.java
@@ -0,0 +1,15 @@
+package Revision;
+
+public class Sum1to10 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+
+            sum = sum + i;
+
+        }
+        System.out.println("Total 1-10:" + sum);
+    }
+}
