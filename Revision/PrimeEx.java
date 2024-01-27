40794a0ca8807fb484d302e163ca646d5ee8b079
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-27 22:17:15 +0600

    commit

---
+++ Revision/PrimeEx.java
@@ -0,0 +1,28 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class PrimeEx {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num, count = 0;
+        System.out.println("Enter a positive number:");
+        num = input.nextInt();
+
+        for (int i = 2; i <= num; i++) {
+            if (num % 2 == 0) {
+                count++;
+                break;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("Prime number");
+
+        } else {
+            System.out.println("Not prime number");
+        }
+
+    }
+}
