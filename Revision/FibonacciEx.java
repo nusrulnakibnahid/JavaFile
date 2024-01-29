2251876311e1a4c8e603c836e4b9969a603b4713
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-29 23:05:19 +0600

    commit

---
+++ Revision/FibonacciEx.java
@@ -0,0 +1,34 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class FibonacciEx {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+
+        int n,first=0,second=1,fibo,total=0;
+        System.out.print("How many Fibonacci numbers you want?:");
+        n = input.nextInt(); 
+
+        System.out.print(first + " " + second  );
+
+
+        for (int i = 3; i <= n; i++){
+
+            fibo = first + second;
+            first = second;
+            second = fibo;
+
+            System.out.print(" " +fibo); 
+            total= total + fibo;
+
+                        
+        }
+        System.out.println();
+
+        System.out.println("Total sum of fibonacci number:"+total);
+       
+
+
+    }
+}
