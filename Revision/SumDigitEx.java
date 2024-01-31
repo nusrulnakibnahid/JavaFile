070b58893beb9f98fceba979f440684bf89d7490
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-01-31 21:18:52 +0600

    commit

---
+++ Revision/SumDigitEx.java
@@ -0,0 +1,25 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class SumDigitEx {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0, r, num, temp;
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+
+        temp = num;
+
+        while (temp != 0) {
+            r = temp % 10;
+            sum = sum + r;
+            temp = temp / 10;
+
+        }
+
+        System.out.println("Sum of digit:" + sum);
+
+    }
+}
