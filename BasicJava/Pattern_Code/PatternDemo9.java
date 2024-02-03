9ab9a7bc7f71952ee88ea9edeeec7aa3d1928eb3
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-03 22:47:36 +0600

    commit

--- BasicJava/Pattern_Code/PatternDemo9.java
+++ BasicJava/Pattern_Code/PatternDemo9.java
@@ -2,7 +2,7 @@ package BasicJava.Pattern_Code;
 
 import java.util.Scanner;
 
-public class PatternDemo {
+public class PatternDemo9 {
     
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in); 
