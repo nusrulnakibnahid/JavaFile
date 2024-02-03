70a7ed55ed4adb3985cdf020f1c074dc845ade9e
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-04 01:31:41 +0600

    commit

--- BasicJava/Pattern_Code/PatternDemo12.java
+++ BasicJava/Pattern_Code/PatternDemo12.java
@@ -7,15 +7,20 @@ public class PatternDemo12 {
         Scanner input = new Scanner(System.in);
         int n;
         System.out.print("Enter line number:");
-        n = input.nextInt(); 
+        n = input.nextInt();
 
         for (int row = n; row >= 1; row--) {
-            
+            // Print leading spaces
+            for (int col = 1; col <= n - row; col++) {
+                System.out.print("  ");
+            }
+
             for (int col = 1; col <= row; col++) {
                 System.out.print(col + " ");
             }
+
             System.out.println();
         }
     }
-    
+
 }
