87ea128983be502403d0eb56a7493788b2abd59b
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-05 00:20:09 +0600

    commit

---
+++ BasicJava/Pattern_Code/PatternDemo15.java
@@ -0,0 +1,24 @@
+package BasicJava.Pattern_Code;
+
+import java.util.Scanner;
+
+public class PatternDemo15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n; 
+        System.out.println("Enter line number:");
+        n = input.nextInt(); 
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == 1 || col == n) {
+                    System.out.print("A");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
