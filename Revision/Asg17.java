05946f0cd88496b298c413ed542f149af1bee476
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-07 22:26:43 +0600

    commit

---
+++ Revision/Asg17.java
@@ -0,0 +1,24 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class Asg17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        System.out.print("Enter line number:");
+        n = input.nextInt(); 
+
+        for (int row =1; row <= n; row++){
+
+            for(int col =1; col <= row; col++){
+                System.out.print(" " + col%2);
+            }
+            System.out.println();
+        }
+
+        
+    }
+    
+}
