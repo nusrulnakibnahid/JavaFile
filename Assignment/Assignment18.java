66f3f07a2c89aaa42d76704cfaf24bfad3b05869
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-16 00:12:02 +0600

    commit

--- Assignment/Assignment18.java
+++ Assignment/Assignment18.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
  * output-> Monday
  */
 public class Assignment18 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner input = new Scanner(System.in);
 
         String[] day = new String[7];  /*
