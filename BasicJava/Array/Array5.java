ee9fa14dc00e3bc0320908d4524311bdf2b09c93
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-22 21:57:32 +0600

    commit

---
+++ BasicJava/Array/Array5.java
@@ -0,0 +1,54 @@
+package BasicJava.Array;
+
+import java.util.Scanner;
+
+public class Array5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[][] A = new int[2][3];
+        int[][] B = new int[2][3];
+
+        // Geting input for A matrix
+        System.out.print("Enter A:");
+        for (int row = 0; row < 2; row++) {
+
+            for (int col = 0; col < 3; col++) {
+                A[row][col] = input.nextInt();
+            }
+
+        }
+
+        // Geting output for A matrix
+        System.out.println("A=");
+        for (int row = 0; row < 2; row++) {
+
+            for (int col = 0; col < 3; col++) {
+                System.out.print(A[row][col] + " ");
+            }
+            System.out.println();
+
+        }
+
+        // Geting input for B matrix
+        System.out.print("Enter B:");
+        for (int row = 0; row < 2; row++) {
+
+            for (int col = 0; col < 3; col++) {
+                A[row][col] = input.nextInt();
+            }
+
+        }
+
+        // Geting output for B matrix
+        System.out.println("B=");
+        for (int row = 0; row < 2; row++) {
+
+            for (int col = 0; col < 3; col++) {
+                System.out.print(A[row][col] + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
