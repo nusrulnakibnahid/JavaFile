8716c6e41a7d14132710ab760865c3d413cbeb79
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-26 21:49:26 +0600

    commit

---
+++ BasicJava/Array/ArrayDemo7.java
@@ -0,0 +1,30 @@
+package BasicJava.Array;
+
+public class ArrayDemo7 {
+    public static void main(String[] args) {
+
+        int[][] number = new int[4][5];
+        int k = 0;
+
+
+        
+        // assing the value of 2D array
+        for (int row = 0; row < 4; row++) {
+
+            for (int col = 0; col < 5; col++) {
+
+                number[row][col] = k;
+                k++;
+            }
+        }
+
+        // Printing the elements
+        for (int row = 0; row < 4; row++) {
+
+            for (int col = 0; col < 5; col++) {
+                System.out.print(number[row][col] + " ");
+            }
+        }
+
+    }
+}
