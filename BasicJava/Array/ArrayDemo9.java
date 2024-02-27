a50254f91e7e75995b6feb6331e8f2bf318d4454
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-27 21:31:02 +0600

    commit

---
+++ BasicJava/Array/ArrayDemo9.java
@@ -0,0 +1,27 @@
+package BasicJava.Array;
+
+import java.util.Arrays;
+
+public class ArrayDemo9 {
+    public static void main(String[] args) {
+
+        int[] number = { 100, 95, 102, -3, 56 };
+
+        Arrays.sort(number);
+
+        // Ascending order of numbers
+        System.out.print("Ascending order:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(" " + number[i]);
+        }
+
+        System.out.println();
+
+        // Decending order of numbers
+        System.out.print("Decending order:");
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(" " + number[i]);
+        }
+
+    }
+}
