e3ce0ee7ed13ca1a6ffd4b89b1fa1ace210f22b1
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-02-28 21:13:24 +0600

    commit

--- BasicJava/Array/ArrayDemo9.java
+++ BasicJava/Array/ArrayDemo9.java
@@ -6,7 +6,6 @@ public class ArrayDemo9 {
     public static void main(String[] args) {
 
         int[] number = { 100, 95, 102, -3, 56 };
-
         Arrays.sort(number);
 
         // Ascending order of numbers
@@ -21,6 +20,27 @@ public class ArrayDemo9 {
         System.out.print("Decending order:");
         for (int i = 4; i >= 0; i--) {
             System.out.print(" " + number[i]);
+        }
+        System.out.println();
+
+
+        
+        String[] name = { "b", "f", "x", "z", "d" };
+        Arrays.sort(name);
+
+        // Ascending order for string
+        System.out.print("Ascending order:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(" " + name[i]);
+        }
+        System.out.println();
+
+
+
+        // Decending order for string
+        System.out.print("Decending order:");
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(" " + name[i]);
         }
 
     }
