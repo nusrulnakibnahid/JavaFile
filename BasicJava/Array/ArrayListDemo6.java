99b3b5b9bff86abe6ce0aa7393ebac3555d72050
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-03-04 00:13:09 +0600

    commit

---
+++ BasicJava/Array/ArrayListDemo6.java
@@ -0,0 +1,30 @@
+package BasicJava.Array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListDemo6 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> number = new ArrayList<>();
+
+        number.add(20);
+        number.add(-3);
+        number.add(18);
+        number.add(92);
+        number.add(0);
+        number.add(2);
+
+        System.out.println("Beforre Sorting (Array List):" + number);
+        System.out.println();
+
+        // sorting(ascending order)
+        Collections.sort(number);
+        System.out.println("After sorting (ascending order):" + number);
+
+        // sorting(descending order)
+        Collections.sort(number, Collections.reverseOrder());
+        System.out.println("After Sorting (descending order):" + number);
+        
+    }
+}
