9939fbba6a86df8543a0772cdb8443da20e2b1cc
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-04-24 23:09:26 +0600

    commit

---
+++ BasicJava/String/StringDemo3.java
@@ -0,0 +1,20 @@
+package BasicJava.String;
+
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String country = "Bangladesh is my country";
+        System.out.println(country);
+
+        char ch = country.charAt(0);
+        System.out.println("Ch"+ch);
+
+        int value = country.codePointAt(ch);
+        System.out.println("value:"+value);
+
+        int pos = country.indexof('n');
+        System.out.println("first position of n:"+pos);
+
+        pos = country.lastIndexof('n');
+        System.out.println("last position of n:"+pos); 
+    }
+}
