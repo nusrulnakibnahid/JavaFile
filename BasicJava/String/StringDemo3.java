e16b65f26d477de2c14a722ff4b20ec972dfd784
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-04-24 23:12:38 +0600

    commit

--- BasicJava/String/StringDemo3.java
+++ BasicJava/String/StringDemo3.java
@@ -2,19 +2,23 @@ package BasicJava.String;
 
 public class StringDemo3 {
     public static void main(String[] args) {
-        String country = "Bangladesh is my country";
+        String country = "        Bangladesh is my country       ";
         System.out.println(country);
 
         char ch = country.charAt(0);
-        System.out.println("Ch"+ch);
+        System.out.println("Ch" + ch);
 
         int value = country.codePointAt(ch);
-        System.out.println("value:"+value);
+        System.out.println("value:" + value);
 
         int pos = country.indexof('n');
-        System.out.println("first position of n:"+pos);
+        System.out.println("first position of n:" + pos);
 
         pos = country.lastIndexof('n');
-        System.out.println("last position of n:"+pos); 
+        System.out.println("last position of n:" + pos);
+
+        // remove space(first & last)
+        String s3 = country.trim();
+        System.out.println(s3);
     }
 }
