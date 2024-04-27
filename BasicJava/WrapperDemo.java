1552bbad478b790869b26a17a4fd0cc0d038ee07
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-04-27 22:30:07 +0600

    commit

--- BasicJava/WrapperDemo.java
+++ BasicJava/WrapperDemo.java
@@ -6,21 +6,24 @@ public class WrapperDemo {
 
     public static void main(String[] args) {
 
-        // premitive -> object
+        //premitive -> object
         int x = 30;
         Integer y = Integer.valueOf(x);
-        System.out.println("y =  " + x);
+        System.out.println("y =  "+x);
 
-        // autoboxing
-        Integer z = x; // Integer.valueOf(x)
-        System.out.println("z = " + z);
+        //autoboxing
+        Integer z = x; //Integer.valueOf(x)
+        System.out.println("z = "+z);
 
-        // object -> premitive data type
+
+        //object -> premitive data type
         Double d = new Double(10.25);
-        System.out.println("d = " + d);
+        System.out.println("d = "+d);
 
-        double e = d.doubleValue(); // or, double e = d;
-        System.out.println("e = " + e);
+        double e = d.doubleValue(); //or, double e = d;
+        System.out.println("e = "+e);
+
+
 
     }
 
