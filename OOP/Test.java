015019e545b9c7576f62cee44ff6a60d785d5217
Md.Nusrul Nakib Nahid <nusrulnakibnahid.mail@gmail.com>
2024-05-03 23:06:26 +0600

    commit

--- OOP/Test.java
+++ OOP/Test.java
@@ -4,7 +4,7 @@ package OOP;
 public class Test {
 
     public static void main(String[] args) {
-        Teacher teacher1;// object declare
+        Teacher teacher1, teacher2;// object declare
         teacher1 = new Teacher();// create
 
         teacher1.name = "Md.Rajib Mia";
@@ -14,6 +14,16 @@ public class Test {
         System.out.println("Name: " + teacher1.name);
         System.out.println("Gender: " + teacher1.gender);
         System.out.println("Phone: " + teacher1.phone);
+
+        teacher2 = new Teacher();// create
+
+        teacher2.name = "Md.Mujib";
+        teacher2.gender = "Male";
+        teacher2.phone = 1234567;
+
+        System.out.println("Name: " + teacher2.name);
+        System.out.println("Gender: " + teacher2.gender);
+        System.out.println("Phone: " + teacher2.phone);
 
     }
 
