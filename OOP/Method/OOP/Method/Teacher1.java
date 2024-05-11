c7fb3fe3d37cd6e8a667346c9246c1d3dd5760a8
Your Name <nusrulnakibnahid.mail@gmail.com>
2024-05-11 21:26:35 +0600

    commit

---
+++ OOP/Method/OOP/Method/Teacher1.java
@@ -0,0 +1,12 @@
+package OOP.Method;
+public class Teacher1 {
+    String name,gender;
+    int phone;
+
+    void displayInformation(){
+
+        System.out.println("Teacher name: "+name);
+        System.out.println("Gender:"+gender);
+        System.out.println("Phone number: "+phone);
+    }
+}
---
+++ OOP/Method/OOP/Method/Test1.java
@@ -0,0 +1,21 @@
+package OOP.Method;
+public class Test1 {
+    public static void main(String[] args) {
+        Teacher1 teacher1, teacher2;// object declare
+        teacher1 = new Teacher1();// create
+
+        teacher1.name = "Md.Rajib Mia";
+        teacher1.gender = "Male";
+        teacher1.phone = 1234567;
+        teacher1.displayInformation();
+
+        teacher2 = new Teacher1();// create
+        teacher2.name = "Md.Mujib";
+        teacher2.gender = "Male";
+        teacher2.phone = 1234567;
+        teacher2.displayInformation();
+
+
+    }
+
+}
